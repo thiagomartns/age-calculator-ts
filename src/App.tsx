44ca30a33c1ed77c8be01fc4f58c4a260214ec84
thiago <tmg3.mn@gmail.com>
2023-05-02 11:28:40 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from './styles/global';
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <GlobalStyles />
+    </div>
+  )
+}
+
+export default App
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+const defaultTheme = {
+  colors: {
+    purple: 'hsl(259, 100%, 65%)',
+    lightRed: 'hsl(0, 100%, 67%)',
+    white: 'hsl(0, 0%, 100%)',
+    offWhite: 'hsl(0, 0%, 94%)',
+    lightGrey: 'hsl(0, 0%, 86%)',
+    smokeyGrey: 'hsl(0, 1%, 44%)',
+    offBlack: 'hsl(0, 0%, 8%)',
+  },
+  typography: {
+    // emptyfornow
+  },
+}
+
+export default defaultTheme;
