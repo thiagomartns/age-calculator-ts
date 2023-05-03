7bddde6953c438d07908df774f01f4275686c123
thiago <tmg3.mn@gmail.com>
2023-05-03 10:21:24 -0300

    Update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import Theme from './theme';
 
 export default createGlobalStyle`
 
@@ -15,5 +16,46 @@ export default createGlobalStyle`
     justify-content: center;
     width: 100vw;
     height: 100vh;
+    background-color: ${Theme.colors.lightGrey};
+
+    .app-content {
+      width: 700px;
+      height: 500px;
+      background-color: #fff;
+      border-radius: 24px 24px 200px 24px;
+      padding: 2rem;
+    }
+  }
+
+  label {
+    text-transform: uppercase;
+    font: ${Theme.typography.poppinsB1};
+    color: ${Theme.colors.smokeyGrey};
+    letter-spacing: .25rem;
+  }
+
+  input {
+    width: 160px;
+    height: 72px;
+    border-radius: 8px;
+    border: none;
+    border: 1px solid ${Theme.colors.lightGrey};
+    padding-left: 1.25rem;
+    font: ${Theme.typography.poppinsB2};
+    
+    &::placeholder {
+      text-transform: uppercase;
+      font: ${Theme.typography.poppinsB2};
+    }
+
+    &:focus {
+      outline: none;
+      border-color: ${Theme.colors.purple};
+      font: ${Theme.typography.poppinsB2};
+    }
+
+    &:focus::placeholder {
+      color: white;
+    }
   }
 `
