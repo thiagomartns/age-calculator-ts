4acd1e27e5e8b94537a2788383c17d4c634c8db6
thiago <tmg3.mn@gmail.com>
2023-05-04 13:02:48 -0300

    Update in h1 logic

--- src/components/AgeCalculator/index.tsx
+++ src/components/AgeCalculator/index.tsx
@@ -10,20 +10,9 @@ const index: React.FC<BirthdayProps> = ({ years, months, days }) => {
 
   return (
     <AgeCalculatorContainer>
-      {years > 0 &&
-        <>
-          <h1>{years} <span>years</span></h1>
-          <h1>{months} <span>months</span></h1>
-          <h1>{days} <span>days</span></h1>
-        </>
-      }
-      {!years &&
-        <>
-          <h1>-- <span>years</span></h1>
-          <h1>-- <span>months</span></h1>
-          <h1>-- <span>days</span></h1>
-        </>
-      }
+      <h1>{years ? years : '--'} <span>years</span></h1>
+      <h1>{months ? months : '--'} <span>months</span></h1>
+      <h1>{days ? days : '--'} <span>days</span></h1>
     </AgeCalculatorContainer>
   )
 }
