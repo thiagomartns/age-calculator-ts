affa3b91221963c0e82598aee182f633532ca501
thiago <tmg3.mn@gmail.com>
2023-05-10 11:05:46 -0300

    Add responsiveness to root component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import Theme from './theme';
+import Media from './media';
 
 export default createGlobalStyle`
 
@@ -14,9 +15,14 @@ export default createGlobalStyle`
     display: flex;
     align-items: center;
     justify-content: center;
-    /* width: 100vw; */
     height: 960px;
     background-color: ${Theme.colors.lightGrey};
+
+    ${Media.mobile} {
+      height: 100vh;
+      align-items: flex-start;
+      padding-top: 6rem;
+    }
 
     .app-content {
       max-width: 840px;
@@ -24,6 +30,15 @@ export default createGlobalStyle`
       background-color: #fff;
       border-radius: 24px 24px 200px 24px;
       padding: 2rem 2rem 4rem 2rem;
+
+      ${Media.mobile} {
+        max-width: 350px;
+        padding: 2rem 1.25rem 4rem 1.25rem;
+        display: flex;
+        flex-direction: column;
+        gap: 2.5rem;
+        border-radius: 24px 24px 100px 24px;
+      }
     }
   }
 
@@ -32,26 +47,45 @@ export default createGlobalStyle`
     font: ${Theme.typography.poppinsB1};
     color: ${Theme.colors.smokeyGrey};
     letter-spacing: .25rem;
+
+    ${Media.mobile} {
+      font: ${Theme.typography.poppinsB4};
+    }
   }
 
   input {
-    max-width: 160px;
+    max-width: 180px;
     height: 72px;
     border-radius: 8px;
     border: none;
     border: 1px solid ${Theme.colors.lightGrey};
     padding-left: 1.25rem;
     font: ${Theme.typography.poppinsB2};
+
+    ${Media.mobile} {
+      max-width: 87.67px;
+      height: 54px;
+      font: ${Theme.typography.poppinsB3};
+      padding-left: .75rem;
+    }
     
     &::placeholder {
       text-transform: uppercase;
       font: ${Theme.typography.poppinsB2};
+
+      ${Media.mobile} {
+        font: ${Theme.typography.poppinsB3};
+      }
     }
 
     &:focus {
       outline: none;
       border-color: ${Theme.colors.purple};
       font: ${Theme.typography.poppinsB2};
+
+      ${Media.mobile} {
+        font: ${Theme.typography.poppinsB3};
+      }
     }
 
     &:focus::placeholder {
@@ -63,6 +97,11 @@ export default createGlobalStyle`
     font: ${Theme.typography.poppinsXB1};
     color: ${Theme.colors.purple};
     font-style: italic;
+
+    ${Media.mobile} {
+      font: ${Theme.typography.poppinsXB2};
+      font-style: italic;
+    }
 
     span {
       color: ${Theme.colors.offBlack};
