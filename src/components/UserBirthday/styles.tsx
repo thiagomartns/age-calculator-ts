889501f3a91cba12c867a6b66820efb583be9d05
thiago <tmg3.mn@gmail.com>
2023-05-10 11:06:05 -0300

    Add responsiveness to UserBirthday component

--- src/components/UserBirthday/styles.tsx
+++ src/components/UserBirthday/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Theme from '../../styles/theme';
+import Media from '../../styles/media';
 
 export const UserBirthdayContainer = styled.div`
   .user-form {
@@ -7,9 +8,18 @@ export const UserBirthdayContainer = styled.div`
     flex-direction: column;
     gap: 2rem;
 
+    ${Media.mobile} {
+      gap: 3rem;
+    }
+
     .form-content {
       display: flex;
       gap: 2rem;
+
+      ${Media.mobile} {
+        gap: 1rem;
+        justify-content: center;
+      }
 
       .input-container {
         display: flex;
@@ -42,6 +52,18 @@ export const UserBirthdayContainer = styled.div`
       position: relative;
       z-index: 1;
       margin-right: 2rem;
+
+      ${Media.mobile} {
+        align-self: center;
+        margin-right: 0;
+        height: 64px;
+        width: 64px;
+
+        img {
+          width: 50%;
+          align-items: center;
+        }
+      }
 
       &:hover {
         background-color: ${Theme.colors.offBlack};
