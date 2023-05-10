738a20415d807286edf60efc9cc371c54491f59e
thiago <tmg3.mn@gmail.com>
2023-05-10 11:06:33 -0300

    Add responsiveness to AgeCalculator component

--- src/components/AgeCalculator/styles.tsx
+++ src/components/AgeCalculator/styles.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import Media from '../../styles/media';
 
 export const AgeCalculatorContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+
+  ${Media.mobile} {
+    gap: .25rem;
+  }
 `
