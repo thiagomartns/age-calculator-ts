df500256cc1bad037541e4a45f9bd3ae35ed119f
thiago <tmg3.mn@gmail.com>
2023-05-10 11:04:23 -0300

    Update global styles for responsive

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,7 +12,10 @@ const defaultTheme = {
     poppinsR1: '400 .875rem/21px "Poppins", sans-serif',
     poppinsB1: '700 .875rem/21px "Poppins", sans-serif',
     poppinsB2: '700 2rem/48px "Poppins", sans-serif',
-    poppinsXB1: '800 6.5rem/100% "Poppins", sans-serif'
+    poppinsB3: '700 1.25rem/30px "Poppins", sans-serif',
+    poppinsB4: '700 .75rem/18px "Poppins", sans-serif',
+    poppinsXB1: '800 6.5rem/100% "Poppins", sans-serif',
+    poppinsXB2: '800 3.5rem/110% "Poppins", sans-serif'
   },
 }
 
